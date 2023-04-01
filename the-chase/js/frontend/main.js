f45eb1b0118c911ada07cb1f87e2f14ae5cbec05
Weizhong <e0260222@u.nus.edu>
2023-04-01 12:37:47 +0800

    Add input handling for lossless decomposition

--- the-chase/js/frontend/main.js
+++ the-chase/js/frontend/main.js
@@ -1,6 +1,15 @@
+function clearFileInput(taskName) {
+  if (taskName === "entailment") {
+    fileForEntailment.value = '';
+  } else if (taskName === "lossless") {
+    fileForLossless.value = '';
+  }
+}
+
 function showFeature(event, taskName) {
   document.getElementById('userInput').style.display = "none";
   document.getElementById('output').style.display = "none";
+  clearFileInput(taskName);
   
   let i, tabContent, tabLinks;
 
@@ -16,4 +25,58 @@ function showFeature(event, taskName) {
 
   document.getElementById(taskName).style.display = "block";
   event.currentTarget.className += " active";
+}
+
+async function convertInputXmlToObj(fileName) {
+  let inputObj = {};
+  let file = document.getElementById(fileName).files[0];
+  if (document.getElementById(fileName).value == "") {
+    alert("Please select an input XML file!");
+    return inputObj;
+  }
+  
+  let text = await file.text();
+  let parser = new XMLParser();
+  inputObj = parser.parse(text);
+  delete inputObj["?xml"];
+
+  let str = JSON.stringify(inputObj, null, 4);
+  console.log(str);
+  return inputObj;
+}
+
+function convertToArray(element) {
+  return element ? [].concat(element) : [];
+}
+
+function createOutputStep(columns, rows) {
+  let step = document.createElement("div");
+  step.className = "step";
+  let description = document.createElement("h3");
+  description.appendChild(document.createTextNode("Sample Description for Step"));
+  step.append(description)
+  let table = document.createElement("table");
+  step.append(table)
+  
+  let tr = document.createElement('tr');
+  table.appendChild(tr);
+  
+  for (let i = 0; i < columns.length; i++) {
+    let th = document.createElement('th');
+    th.appendChild(document.createTextNode(columns[i]));
+    tr.appendChild(th);
+  }
+  
+  for (let i = 0; i < rows.length; i++) {
+    let tr = document.createElement('tr');
+    table.appendChild(tr);
+    
+    for (let j = 0; j < rows[i].length; j++) {
+      let td = document.createElement('td');
+      td.appendChild(document.createTextNode(rows[i][j]));
+      tr.appendChild(td);
+    }
+  }
+    
+  document.getElementById("output").append(step);
 }
